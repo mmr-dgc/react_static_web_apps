77622e131086f88b05ff44f0ca1de10f2ad185fe
Mamoru Deguchi <mmr.deguchi@gmail.com>
2022-03-17 23:46:43 +0900

    create negotiate

---
+++ api/negotiate/index.ts
@@ -0,0 +1,9 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, connectionInfo: any): Promise<void> {
+    context.res = {
+        body: connectionInfo
+    };
+};
+
+export default httpTrigger;
