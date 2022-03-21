73a3800118694e725f867146bb0bc643d514ce80
Mamoru Deguchi <mmr.deguchi@gmail.com>
2022-03-21 20:01:14 +0900

    fix negotiate

--- api/negotiate/index.ts
+++ api/negotiate/index.ts
@@ -1,7 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, connectionInfo: any): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, connectionInfo: any): Promise<void> {
     context.res = {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Credentials":true,
+            "Access-Control-Expose-Headers": "x-signalr-user-agent",
+            "Access-Control-Allow-Headers": "x-requested-with, x-signalr-user-agent"
+        },
         body: connectionInfo
     };
 };
